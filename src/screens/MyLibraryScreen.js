047edfb253f91c5642abc1bb3a4d853f30967a99
milowedo <miloszszwedo@gmail.com>
2019-12-03 23:21:09 +0100

    mocked a simple list

--- src/screens/MyLibraryScreen.js
+++ src/screens/MyLibraryScreen.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import {Text, View} from "react-native";
+import {FlatList, Text, TouchableOpacity, View} from "react-native";
+import NavigationEvents from "@react-navigation/core/src/views/NavigationEvents";
+import {Button, ListItem} from "react-native-elements";
 
 
 const MyLibraryScreen = () => {
+    const mock = [
+        {_id: "1", name: "Kurt Vonnegut"},
+        {_id: "2", name: "Kurt Vonnegut"},
+        {_id: "3", name: "Kurt Vonnegut"},
+    ];
     return (
-        <View>
-            <Text>Library Screen </Text>
-        </View>
+        <>
+            <NavigationEvents onWillFocus={() => {
+                console.log("TO DO: on focus fetch from remote and check if data up to date")
+            }}/>
+            <FlatList
+                data={mock}
+                keyExtractor={item => item._id}
+                renderItem={({item}) => {
+                    return <TouchableOpacity
+                        onPress={() => {
+                            console.log("TO DO: on click of the position do something ugh")
+                        }}>
+                        <ListItem chevron title={item.name}/>
+                    </TouchableOpacity>
+                }}/>
+
+            <Button title={"Go to Track detail"} onPress={() => {
+                navigation.navigate('TrackDetail')
+            }}/>
+        </>
     );
 };
 
