1dfb3e3b50b97f9475c9200593816bdb1c566216
milowedo <miloszszwedo@gmail.com>
2019-12-26 13:47:37 +0100

    Shelves in the application are stored in remote mongoose

--- src/context/BooksContext.js
+++ src/context/BooksContext.js
@@ -2,45 +2,8 @@ import React from "react";
 import AbstractDataContext from "./AbstractDataContext";
 import {AsyncStorage} from "react-native";
 
-let wantedBooks = [
-    {_id: "0", writer: "Kurt Vonnegut", title: "Breakfast of Champions", price: 37},
-    {_id: "1", writer: "Truman Capote", title: "Z zimną krwią", price: 55},
-    {_id: "2", writer: "Philip Kindred Dick", title: "Valis", price: 12},
-    {_id: "3", writer: "J. Sartre", title: "Słowa", price: 1},
-    {_id: "4", writer: "Janusz Zajdel", title: "Prawo do powrotu", price: 99},
-    {_id: "5", writer: "Pratchett", title: "Kolor magii", price: 6},
-    {_id: "6", writer: "Tolkien", title: "Silmarilion", price: 9},
-    {_id: "7", writer: "Michał Bułhakov", title: "Mistrz i Małgorzata", price: 3},
-    {_id: "8", writer: "Ernest Hemingway", title: "Słońce też wschodzi", price: 39},
-    {_id: "9", writer: "Nietzsche", title: "Ecce homo", price: 44},
-    {_id: "10", writer: "Andrzej Sapkowski", title: "Krew Elfów", price: 1},
-
-    {
-        _id: "11",
-        writer: "Alexandra Elizabeth Sheedy",
-        title: "She Was Nice To Mice: The Other Side of Elizabeth I's Character Never Before Revealed by Previous Historians",
-        price: 32
-    },
-];
-let myBooks = [
-    {_id: "0", writer: "Kurt Vonnegut", title: "Slaughterhouse no 5"},
-    {
-        _id: "1",
-        writer: "Philip Gourevitch",
-        title: "We Wish to Inform You That Tomorrow We Will Be Killed with Our Families"
-    },
-    {_id: "2", writer: "Philip Kindred Dick", title: "Do Androids Dream of Electric Sheep?"},
-    {_id: "3", writer: "Jerome K. Jerome", title: "Trzech panów w łódce (nie licząc psa)"},
-    {_id: "4", writer: "James Jones", title: "The Thin Red Line"},
-    {_id: "5", writer: "William Golding", title: "Widzialna ciemność"},
-    {_id: "6", writer: "Luke Dittrich", title: "Eksperyment"},
-    {_id: "7", writer: "Franz Kafka", title: "Proces"},
-    {_id: "8", writer: "Niccolò Machiavelli", title: "Książe"},
-    {_id: "9", writer: "James Clavell", title: "Król szczurów"},
-    {_id: "10", writer: "Norman Davies", title: "Boże igrzysko"},
-    {_id: "11", writer: "Jaume Cabré", title: "Głosy panamo"},
-    {_id: "12", writer: "Jaume Cabré", title: "Wyznaję"},
-];
+let wantedBooks = null;
+let myBooks = null;
 const gateway = "https://gb-gateway.herokuapp.com";
 
 const booksReducer = (state, action) => {
@@ -80,17 +43,36 @@ const booksReducer = (state, action) => {
     }
 };
 
-const fetchMyBooks = dispatch => () => {
+const fetchMyBooks = dispatch => async () => {
+    if (!myBooks) await fetchShelves();
     dispatch({type: 'get_my_books', payload: myBooks})
 };
 const deleteMyBook = dispatch => (id) => {
     dispatch({type: 'delete_my_book', payload: id})
 };
 
+async function fetchShelves() {
+    const response = await fetch(`${gateway}/books`, {
+        method: 'GET',
+        headers: {
+            "content-type": 'application/json',
+            "authorization": `Bearer ${await AsyncStorage.getItem('token')}`
+        },
+    });
+    if (!response.ok) {
+        console.log("Error response while fetching books");
+        return;
+    }
+    const responseObject = await response.json();
+    console.log("Received response with fetched shelves");
+    myBooks = responseObject.library;
+    wantedBooks = responseObject.wanted;
+}
+
 async function persistBooks(shelfType) {
     const body = shelfType === "library" ? {library: myBooks} : shelfType === "wanted" ? {wanted: wantedBooks} : null;
     if (body == null) return;
-    const response = await fetch(`${gateway}/books`, {
+    const response = fetch(`${gateway}/books`, {
             method: 'POST',
             headers: {
                 "content-type": 'application/json',
@@ -110,7 +92,8 @@ const addBookToLibrary = dispatch => async (title, writer) => {
     persistBooks("library");
 };
 
-const fetchWantedBooks = dispatch => () => {
+const fetchWantedBooks = dispatch => async () => {
+    if (!wantedBooks) await fetchShelves();
     dispatch({type: 'get_wanted_books', payload: wantedBooks})
 };
 const deleteWantedBook = dispatch => (id) => {
