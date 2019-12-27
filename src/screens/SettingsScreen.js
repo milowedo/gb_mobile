45f5bc4fcd77e0a5162c6a3302f4a985d2615f1d
milowedo <miloszszwedo@gmail.com>
2019-12-27 21:53:54 +0100

    settings screen brushed 🧹

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {Button, Icon, Text} from "react-native-elements";
 import {headerStyles} from "../constants/Layouts";
 import {navigate} from "../utils/navigationHelper";
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
 import MarginWrapper from "../components/utilities/MarginWrapper";
 import {Context} from "../context/AuthenticationContext";
 
@@ -11,12 +11,11 @@ const SettingsScreen = ({navigation}) => {
     const {signout} = useContext(Context);
 
     return (
-        <>
-            <Text h2>Woah settings screen</Text>
+        <View style={{flex:1, alignItems:'center', paddingHorizontal: 50, marginTop: 100}}>
             <MarginWrapper>
                 <Button title={"Sign out"} onPress={signout}/>
             </MarginWrapper>
-        </>
+        </View>
     )
 };
 
